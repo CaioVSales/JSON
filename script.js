7ed7b3721e7d51aaec5b51c52f987dcdc3df5b66
Caio <caiovictorsalesdefreitas@gmail.com>
2022-06-27 21:51:09 -0300

    Finish

---
+++ script.js
@@ -0,0 +1,24 @@
+const objects = [
+    {
+        nome: 'Caio',
+        idade: 19,
+        esta_trabalhando: false,
+        detalhes_profissao: {
+            profissao: 'Programador',
+            empresa: null,
+        },
+        hobbies: ['Programar', 'Malhar', 'Dormir'],
+    },
+    {
+        nome: 'Eduardo',
+        idade: 19,
+        esta_trabalhando: true,
+        detalhes_profissao: {
+            profissao: 'Programador',
+            empresa: "LIV Saude",
+        },
+        hobbies: ['Jogar', 'Academia'],
+    },
+]
+
+console.log(objects)
